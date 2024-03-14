28ad1b713fd0bc5b5ad891306ab8787c37f126de
farya <fdash@yandex.ru>
2024-03-14 16:36:48 +0300

    second commit

---
+++ src/main/GamePanel.java
@@ -0,0 +1,74 @@
+package main;
+
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class GamePanel extends JPanel {
+
+    private MouseInputs mouseInputs;
+    private float xDelta = 100, yDelta = 100;
+    private BufferedImage img, subImg;
+
+    public GamePanel() {
+
+        mouseInputs = new MouseInputs(this);
+
+        importImg();
+
+        setPanelSize();
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+
+    }
+
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
+    }
+
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+        repaint();
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+        repaint();
+    }
+
+    public void setRectPos(int x, int y) {
+        this.xDelta = x;
+        this.yDelta = y;
+        repaint();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        subImg = img.getSubimage(1*64, 8*40, 64, 40);
+        g.drawImage(subImg, (int) xDelta, (int) yDelta, 128, 80, null);
+
+    }
+
+}
